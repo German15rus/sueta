08a0928eb4cbcac350239cf2b4058847c7e354c7
tibilov <usasmen123@mail.ru>
2025-12-03 23:29:09 +0300

    dz 03.12

--- sueta/Program.cs
+++ sueta/Program.cs
@@ -6,103 +6,60 @@ namespace sueta
     {
         static void Main(string[] args)
         {
-            //string word = Console.ReadLine();
+            //int[] massive1 = { 1, 2, 3, 4 };
+            //int[] massive2 = { 3, 4, 5, 6 };
+            //int[] finalmassive = ConcatenateDistinct(massive1, massive2);
+            //int min = Convert.ToInt32(Console.ReadLine());
+            //int max = Convert.ToInt32(Console.ReadLine());
             //int num = Convert.ToInt32(Console.ReadLine());
-            //int[] numbers = [1, 2, 3, 4, 7];
-            char s = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine(TranslationCapitalLetter(s));
+            //IsInRange(num, min, max);
+            //string str1 = Console.ReadLine()!;
+            //string str2 = Console.ReadLine()!;
+            //CheckStrings(str1, str2);
+            //int paskal = Convert.ToInt32(Console.ReadLine());
+            //PaskalTriangle(paskal);
         }
-        static bool CheckParity(int num)//1
-        {
-            bool check = false;
-            if (num % 2 == 0)
-            {
-                check = true;
+        //public static string CheckStrings(string str1, string str2)//17Для двух строк напишите метод, определяющий, является ли одна строка перестановкой другой
+        //{
+        //    if (str1.Length != str2.Length)
+        //        return "Не является";
+        //    char[] chars1 = str1.ToCharArray();
+        //    char[] chars2 = str2.ToCharArray();
+        //    Array.Sort(chars1);
+        //    Array.Sort(chars2);
 
-            }
-            return check;
+        //    bool CorrectString = new string(chars1) == new string(chars2);
+        //    if (CorrectString)
+        //        return "Является";
+        //    else
+        //        return "не является";
+        //}
+        //static void PaskalTriangle(int row)//14Написать функцию, выводящую в консоль треугольник Паскаля
+        //{
+        //    for (int i = 0; i < row; i++)
+        //    {
+        //        Console.Write(new string(' ', (row - i - 1) * 3));
 
-        }
-        static int Factorial(int num)//2
-        {
-            int factorial = 1;
-            for (int i = 1; i<=num; i++)
-            {
-                factorial = i * factorial;
-            }
-            return factorial;
-        }
-        static int CountVowels(string word)//3
-        {
-            string vowels = "aeiouy";
-            int countVowels = 0;
-            foreach (char c in word)
-            {
-				if (vowels.Contains(c))
-				{
-					countVowels++;
-				}
-			}
-            return countVowels;
-        }
-        static bool FindPolindrom(string word)//4
-        {
-            bool polindrom = false;
-            string newString = "";
-            for (int i = word.Length - 1;i >= 0; i--)
-            {
-                newString.Append(word[i]);
-            }
-            if (newString == word)
-            {
-                polindrom = true;
-            }
-            Console.Write(newString + " ");
-            return polindrom;
-        }
-        static int FindMax(int[] numbers)//5
-        {
-			int max = numbers[1];
-			for (int i = 0; i <= numbers.Length - 1; i++)
-            {
-                if (numbers[i] > max)
-                    max = numbers[i];
-            }
-            return max;
-        }
-        static bool FindPrimeNum(int num)//6
-        {
-            bool primeNum = false;
-            if (num < 2)
-                return primeNum;
-            for (int i = 2; i * i <= num; i++)
-            {
-                if (num % i == 0)
-                {
-                    return primeNum;
-                }
-            }
-            primeNum = true;
-            return primeNum;
-        }
-        static char TranslationCapitalLetter(char s)//7
-        {
-            char[] Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        //        int number = 1;
+        //        for (int j = 0; j <= i; j++)
+        //        {
+        //            Console.Write($"{number,6}");
+        //            number = number * (i - j) / (j + 1);
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+        //public static bool IsInRange(int number, int min, int max)//5Написать функцию проверки, находится ли число в заданном диапазоне.
 
-			for (int i = 0; i < Letters.Length - 1; i++)
-            {
-                if (s == Letters[i])
-                {
-                    s = char.ToUpper(s);
-                }
-            }
-            return s;
-        }
-        static string MakeCamelCase(string word)
-        {
-            string newString = "";
+        //{
+        //    return number >= min && number <= max;
+        //}
+        //public static Massive[] ConcatenateDistinct<Massive>(Massive[] array1, Massive[] array2)//8Написать функцию, которая принимает два массива и возвращает их конкатенацию
+        //{
+        //    if (array1 == null) throw new ArgumentNullException(nameof(array1));
+        //    if (array2 == null) throw new ArgumentNullException(nameof(array2));
 
-
+        //    return array1.Concat(array2).Distinct().ToArray();
         }
     }
 }
